5a5eb71c142050f6b31a38d2a76be7e14c5008aa
Bhumika Rathore <bhumikar0718@gmail.com>
2025-10-09 12:32:57 +0200

    changes

--- Model/Tile.java
+++ Model/Tile.java
@@ -1,4 +1,35 @@
 package Model;
+import java.util.UUID;
+
 public class Tile {
-    
+    private int value;
+    private String id;
+    private boolean merged;
+
+    //constructor
+    public Tile(int value) {
+        this.value = 0;
+        //this.id = UUID.randomUUID().toString(); //to create unique id and and to convrt it into
+        // string 
+        this.merged = false;
+    }
+
+    boolean isEmpty(){
+        return value == 0;
+    }
+
+    void reset() {
+        value = 0;
+        merged = false;
+    }
+
+    void setValue(int newValue) {
+
+    }
+
+
 }
+
+
+    
+
