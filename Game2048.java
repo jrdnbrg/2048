e25f48cb2e27cdcede6ff742d8c47ad58e02a170
Jeske Rodenburg <jeskerodenburg3@gmail.com>
2025-10-09 10:52:29 +0200

    "Show grid"

---
+++ Game2048.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public class Game2048 {
+    
+    public Game2048() {
+        Board gridGrame = new Board();
+    }
+
+    public static void main(String[] args) {
+        new Game2048();
+    } 
+}
---
+++ TileLabel.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+// Probably change to JPanel so we can draw on it
+public class TileLabel extends JLabel {
+    private int value;
+    
+    TileLabel() {
+        setOpaque(true);
+        setBackground(Color.WHITE);
+
+        value = 0;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void displayTile() {
+        // Change to display the actual drawn tile
+        if (value != 0) {
+            setText("" + value);
+        }
+    }
+}
+ 
