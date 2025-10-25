a6cd5fa23880b3f13442ba655b9ceae3c1420a18
Bhumika Rathore <bhumikar0718@gmail.com>
2025-10-25 15:14:11 +0200

    Changes in Add icon

--- Game2048.java
+++ Game2048.java
@@ -17,7 +17,7 @@ public class Game2048 {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        ImageIcon image = new ImageIcon("View/icon.png"); //create an image icon 
+        ImageIcon image = new ImageIcon(Game2048.class.getResource("View/icon.png")); //create an image icon 
         frame.setIconImage(image.getImage());
         frame.setVisible(true);
     } 
